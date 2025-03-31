223cfe0b3eb7a76b1a6b29f392d5edfa93b4f528
ruslan-parhomenko <parhomenkogm@gmail.com>
2025-03-31 18:21:47 +0300

    fix

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"NBrxzLOIf35bGQzHN/ACCLAbeztGvz37/cStpcKuiis=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  distDir: 'build',
   images: {
     unoptimized: true
   }
